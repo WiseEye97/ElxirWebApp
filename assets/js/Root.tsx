55eb830cea778fe85cf152354b9b4fd375c5d7dc
WiseEye97 <arekgpn@gmail.com>
2019-10-20 20:15:01 +0200

    init

---
+++ assets/js/Root.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import HomePage from './pages'
+
+export default class Root extends React.Component {
+    public render(): JSX.Element {
+      return (
+        <>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                </Switch>
+            </BrowserRouter>
+        </>
+      )
+    }
+  }
